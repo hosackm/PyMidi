a36f6d7bac931825573fe83d548d60ab7f589478
hosackm <hosack.matt@gmail.com>
2015-06-25 14:09:32 -0700

    Added documentation for Input class.  Added _pymidi.py to gitignore

--- pymidi.py
+++ pymidi.py
@@ -45,29 +45,6 @@ class PmEvent():
         self.message = message
         self.timestamp = timestamp
 
-    @classmethod
-    def events_from_buffer(cls, buf, gen=False):
-        '''Returns a list of PmEvent instances
-        from a buffer of cdata PmEvent structs from the cffi interface'''
-        return [cls(event.message) for event in buf]
-
-    @classmethod
-    def create_note_on(cls, key, velocity=0):
-        '''create_note_on(key) returns a Note On PmEvent with velocity = 0
-        create_note_on(key, velocity) returns a Note On PmEvent'''
-        message = ((0x90 << 16) & 0xFF0000)
-        message |= ((key << 8) & 0xFF00)
-        message |= (velocity & 0xFF)
-        return cls(message, 0)
-
-    @classmethod
-    def create_note_off(cls, key):
-        '''Returns a Note Off PmEvent instance'''
-        message = ((0x80 << 16) & 0xFF0000)
-        message |= ((key << 8) & 0xFF00)
-        message |= 0x7F
-        return cls(message)
-
     def get_status(self):
         'Return the status of this PmEvent'
         return self.message & 0xFF
@@ -96,6 +73,29 @@ class PmEvent():
         Otherwise returns False'''
         return self.get_status() == 0xB0
 
+    @classmethod
+    def events_from_buffer(cls, buf, gen=False):
+        '''Returns a list of PmEvent instances
+        from a buffer of cdata PmEvent structs from the cffi interface'''
+        return [cls(event.message) for event in buf]
+
+    @classmethod
+    def create_note_on(cls, key, velocity=0):
+        '''create_note_on(key) returns a Note On PmEvent with velocity = 0
+        create_note_on(key, velocity) returns a Note On PmEvent'''
+        message = ((0x90 << 16) & 0xFF0000)
+        message |= ((key << 8) & 0xFF00)
+        message |= (velocity & 0xFF)
+        return cls(message, 0)
+
+    @classmethod
+    def create_note_off(cls, key):
+        'Returns a Note Off PmEvent instance'
+        message = ((0x80 << 16) & 0xFF0000)
+        message |= ((key << 8) & 0xFF00)
+        message |= 0x7F
+        return cls(message)
+
     def __str__(self):
         return repr(self)
 
@@ -103,23 +103,42 @@ class PmEvent():
         return 'PmEvent(Status: {}, Note: {}, Velocity: {})'.format(
             self.get_status(),
             self.get_key(),
-            self.get_vel())
+            self.get_velocity())
+
+    def __eq__(self, other):
+        '''PmEvents are considered equal if they have
+        the same message and timestamp'''
+        return ((self.message == other.message) and
+                (self.timestamp == self.timestamp))
+
+    def deepcopy(self):
+        'Return a deep copy of this object.  Not just a reference'
+        return self.__class__(self.message, self.timestamp)
 
 
 class Input(object):
     '''_'''
     PNULL = ffi.new('void **')[0]
 
-    def __init__(self, device_id, buffer_size=4096):
-        '''_'''
+    def __init__(self, device_id=None, buffer_size=4096):
+        '''Initialize a new MIDI Input Stream and open it
+        If the stream fails to open a MidiException will be raised.
+
+        :param device_id
+        :param buffer_size is the num of PmEvents that can be stored at a time
+        '''
+        if device_id is None:
+            device_id = lib.Pm_GetDefaultInputDevice()
         if device_id < 0:
             raise MidiException('Device ID must be a positive integer')
+
         if not _is_instantiated():
             _instantiate_library()
 
         self.buffer_size = buffer_size
         self.buffer = ffi.new('PmEvent []', buffer_size)
         pp_stream = ffi.new('PortMidiStream **')
+
         err = lib.Pm_OpenInput(pp_stream,
                                device_id,
                                self.PNULL,
@@ -128,10 +147,13 @@ class Input(object):
                                self.PNULL)
         if err:
             raise MidiException(err)
+
         self.stream = pp_stream[0]
 
     def read(self, num_events=4096):
-        '''_'''
+        '''Read MIDI events and return a list of PmEvent instances
+        :param num_events the number of events to read
+        :rtype list of PmEvents read from MIDI Input'''
         num_events = min(num_events, self.buffer_size)
         ret = lib.Pm_Read(self.stream, self.buffer, num_events)
         if ret < 0:
@@ -141,7 +163,8 @@ class Input(object):
         return PmEvent.events_from_buffer(self.buffer[s])
 
     def poll(self):
-        '''_'''
+        '''Returns True if events are ready otherwise False.
+        Raises an exception if there was an error calling the PmPoll()'''
         ret = lib.Pm_Poll(self.stream)
         if ret == 1:
             return True
@@ -151,6 +174,7 @@ class Input(object):
             raise MidiException(ret)
 
     def close(self):
+        'Closes the MIDI Input Stream'
         lib.Pm_Close(self.stream)
 
 
@@ -160,13 +184,13 @@ class Output(object):
 
 
 def _is_instantiated():
-    '''Return True if Portmidi is already open else False'''
+    'Return True if Portmidi is already open else False'
     global SINGLETON
     return SINGLETON is not None
 
 
 def _instantiate_library():
-    '''Initialize Portmidi library'''
+    'Initialize Portmidi library'
     global SINGLETON
     SINGLETON = ffi.new('PortMidiStream **')
     num = lib.Pm_Initialize()
