be32bfe1e50712a4c6bf280bd98906185fd4b630
hosackm <hosack.matt@gmail.com>
2015-06-25 17:21:22 -0700

    Added code so that the portmidi header that ships with portmidi can be used and not my preprocessed version

--- build_pymidi.py
+++ build_pymidi.py
@@ -4,10 +4,18 @@ from cffi import FFI
 
 __all__ = ('ffi',)
 
+# Modify this location to support Linux and Windows
+header = '/usr/local/Cellar/portmidi/217/include/portmidi.h'
 
-p = subprocess.Popen(['clang -E include/portmidi.h'],
+# Run the preprocessor on the header
+p = subprocess.Popen(['clang -E {}'.format(header)],
                      shell=True, stdout=subprocess.PIPE)
 code = p.communicate()[0]
+
+# These two sections of the header make cffi cough up.  They aren't used
+# by portmidi.
+code = code.replace('__signed', 'signed')
+code = code.replace('typedef __builtin_va_list __darwin_va_list;', '')
 
 ffi = FFI()
 ffi.cdef(code)
